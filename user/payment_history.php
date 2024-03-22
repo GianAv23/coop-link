dd939e9db45f1da7cea1e8ff5268a6d233729291
Gian Avila <avila.gian111@gmail.com>
2024-03-23 01:01:26 +0700

    [add]: responsive layout payment_history page

--- user/payment_history.php
+++ user/payment_history.php
@@ -26,8 +26,7 @@
 </head>
 
 <body>
-    <div class="bg-bgBlack w-screen min-h-screen">
-
+    <div class="bg-bgBlack w-screen min-h-screen px-10 overflow-hidden">
 
         <!-- NAVBAR START -->
         <div class="flex flex-row px-10 justify-between pt-10">
@@ -42,109 +41,85 @@
                 <span class="text-white font-semibold ml-5">
                     Admin
                 </span>
+
                 <div class="ml-3 w-7 h-7 rounded-full border border-bgWhite bg-cover bg-center"
-                    style="background-image: url()">
+                    style="background-image: url(assets/test.jpg);">
                 </div>
+
+
             </div>
         </div>
         <!-- NAVBAR END -->
 
         <!-- BODY START -->
         <div class="flex flex-row justify-between">
-            <div class="w-full h-screen bg-bgBlack rounded-3xl border border-textColor2 m-8">
-                <div class="flex flex-col p-6 gap-4">
-
-                    <div class="mb-3">
-                        <span class="font-normal text-bgWhite text-lg">User Registration
+            <div class="w-full h-full bg-bgBlack rounded-3xl border border-textColor2 m-8">
+                <div class="flex flex-col p-6 gap-4 shrink-0">
+                    <div class="mb-3 flex items-center gap-4">
+                        <!-- untuk kembali ke home -->
+                        <button class="rounded-full w-8 h-8 bg-bgLogo p-2 flex items-center justify-center"><span
+                                class="font-bold text-xl">
+                                < </span></button>
+                        <span class="font-normal text-bgWhite text-lg lg:text-2xl">User Registration
                             <span class="text-textColor2 font-bold">Verification</span></span>
                     </div>
 
                     <!-- INI YANG DI LOOP PID -->
-                    <div
-                        class="bg-cardData/50 w-full h-full rounded-3xl border border-bgWhite flex flex-col p-3 backdrop-blur-md">
-                        <div class="flex flex-col">
-                            <div class="flex flex-col gap-0">
-                                <span class="font-semibold text-bgWhite text-lg">
-                                    value nama nasabah
-                                </span>
-                                <span class="font-normal text-bgWhite text-sm text-textColor2">
-                                    value email nasabah
-                                </span>
-                                <span class="font-normal text-bgWhite text-sm">
-                                    value tanggal pembayaran | value alamat
-                                </span>
-                            </div>
+                    <div class="h-[600px] flex-grow overflow-y-scroll flex flex-col gap-4">
+                        <div
+                            class="bg-cardData/50 rounded-3xl border border-bgWhite flex flex-col p-4 backdrop-blur-md">
+                            <div class="flex flex-col md:flex-row md:justify-between md:items-start">
 
-                            <div class="mt-6 flex flex-row justify-between">
-                                <button class="rounded-xl bg-bgColor border border-textColor2 px-4 py-1">
-                                    <span class="font-medium text-textColor2 text-sm">Transfer Proof</span>
-                                </button>
-
-                                <div class="flex flex-row gap-2">
-                                    <button class="rounded-xl bg-bgLogo px-4 py-1">
-                                        <span class="font-medium text-bgColor text-sm">Accept</span>
-                                    </button>
-
-
-                                    <button class="rounded-xl border border-textColor2 px-4 py-1">
-                                        <span class="font-medium text-bgWhite text-sm">Reject</span>
-                                    </button>
+                                <div class="flex flex-col gap-0">
+                                    <span class="font-semibold text-bgWhite text-lg">
+                                        value nama nasabah
+                                    </span>
+                                    <span class="font-normal text-bgWhite text-sm text-textColor2">
+                                        value email nasabah
+                                    </span>
+                                    <span class="font-normal text-bgWhite text-sm">
+                                        value tanggal pembayaran | value alamat
+                                    </span>
                                 </div>
 
-                            </div>
-                        </div>
-                        <!-- INI YANG DI LOOP PID -->
-
-
-                    </div>
-
-                    <div
-                        class="bg-cardData/50 w-full h-full rounded-3xl border border-bgWhite flex flex-col p-3 backdrop-blur-md">
-                        <div class="flex flex-col">
-                            <div class="flex flex-col gap-0">
-                                <span class="font-semibold text-bgWhite text-lg">
-                                    value nama nasabah
-                                </span>
-                                <span class="font-normal text-bgWhite text-sm text-textColor2">
-                                    value email nasabah
-                                </span>
-                                <span class="font-normal text-bgWhite text-sm">
-                                    value tanggal pembayaran | value alamat
-                                </span>
-                            </div>
-
-                            <div class="mt-6 flex flex-row justify-between">
-                                <button class="rounded-xl bg-bgColor border border-textColor2 px-4 py-1">
-                                    <span class="font-medium text-textColor2 text-sm">Transfer Proof</span>
-                                </button>
-
-                                <div class="flex flex-row gap-2">
-                                    <button class="rounded-xl bg-bgLogo px-4 py-1">
-                                        <span class="font-medium text-bgColor text-sm">Accept</span>
+                                <div class="mt-6 flex flex-row justify-between md:gap-10 md:pb-4">
+                                    <button class="rounded-xl bg-bgColor border border-textColor2 px-4 py-1">
+                                        <span class="font-medium text-textColor2 text-xs md:text-sm">Transfer
+                                            Proof</span>
                                     </button>
 
+                                    <div class="flex flex-row gap-2">
+                                        <button class="rounded-xl bg-bgLogo px-4 py-1">
+                                            <span class="font-medium text-bgColor text-xs md:text-sm">Accept</span>
+                                        </button>
 
-                                    <button class="rounded-xl border border-textColor2 px-4 py-1">
-                                        <span class="font-medium text-bgWhite text-sm">Reject</span>
-                                    </button>
+
+                                        <button class="rounded-xl border border-textColor2 px-4 py-1">
+                                            <span class="font-medium text-bgWhite text-xs md:text-sm">Reject</span>
+                                        </button>
+                                    </div>
+
                                 </div>
-
                             </div>
+                            <!-- INI YANG DI LOOP PID -->
+                            <!-- </div> -->
                         </div>
-                        <!-- INI YANG DI LOOP PID -->
 
-
+                        <!-- BATASSSSSSS -->
                     </div>
                 </div>
-            </div>
-        </div>
-        <!-- BODY END -->
 
-        <div class="flex flex-row justify-between">
-            <div class=" w-full px-10 py-2 rounded-full border border-bgWhite mx-8 mb-4 flex justify-center">
-                <span class="text-sm text-bgWhite">2024 | UTS Web Programming</span>
+
             </div>
         </div>
+    </div>
+    <!-- BODY END -->
+
+    <!-- <div class="flex justify-center">
+        <div class="fixed bottom-0 w-96 py-2 rounded-full border border-bgWhite mb-4 flex items-center justify-center">
+            <span class="text-sm text-bgWhite">2024 | UTS Web Programming</span>
+        </div>
+    </div> -->
     </div>
 
 
